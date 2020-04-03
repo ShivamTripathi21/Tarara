90a8b71ddab7517660b59300b416bfa17aade486
Shivam Tripathi <stripathi3397@gmail.com>
2020-04-03 19:14:12 +0530

    removed search bar

--- Web/src/_components/Header/Header.js
+++ Web/src/_components/Header/Header.js
@@ -6,10 +6,7 @@ export default function Header(){
     
     return(
         <header className="header-class">
-            <ReactSearchBox
-                placeholder="Placeholder"
-                value="Doe"
-            />
+            
         </header>
     );
 }
